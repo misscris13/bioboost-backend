9675b7232775eeb8b1a60040f1a23ee4c5486c1c
Cristal <cristalcamposabad@gmail.com>
2023-11-09 12:49:04 +0100

    init

---
+++ bioboost/src/main/java/com/cdtm/bioboost/BioboostApplication.java
@@ -0,0 +1,13 @@
+package com.cdtm.bioboost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioboostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioboostApplication.class, args);
+	}
+
+}
---
+++ bioboost/src/test/java/com/cdtm/bioboost/BioboostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdtm.bioboost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioboostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
