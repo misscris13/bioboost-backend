cdb386483b219fae988b4af49e9364a853a6d974
Cristal <cristalcamposabad@gmail.com>
2023-11-16 12:08:15 +0100

    1.0. User REST: testing done

--- bioboost/src/test/java/com/cdtm/bioboost/user/UserIT.java
+++ bioboost/src/test/java/com/cdtm/bioboost/user/UserIT.java
@@ -1,13 +1,17 @@
 package com.cdtm.bioboost.user;
 
+import com.cdtm.bioboost.user.model.User;
 import com.cdtm.bioboost.user.model.UserDto;
+import org.apache.coyote.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 
@@ -38,5 +42,81 @@ public class UserIT {
 
         assertNotNull(response);
         assertEquals(2, response.getBody().size());
+    }
+
+    public static final Long NEW_USER_ID = 12L;
+    public static final String NEW_USER_NAME = "u3";
+    public static final String NEW_USER_PASS = "p3";
+
+    @Test
+    public void saveWithoutIdShouldCreateNewUser() {
+
+        UserDto dto = new UserDto();
+        dto.setUser(NEW_USER_NAME);
+        dto.setPassword(NEW_USER_PASS);
+
+        restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.PUT, new HttpEntity<>(dto), Void.class);
+
+        ResponseEntity<List<UserDto>> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.GET, null, responseType);
+        assertNotNull(response);
+        assertEquals(3, response.getBody().size());
+
+        UserDto userSearch = response.getBody().stream().filter(item -> item.getId().equals(NEW_USER_ID)).findFirst().orElse(null);
+        assertNotNull(userSearch);
+        assertEquals(NEW_USER_NAME, userSearch.getUser());
+    }
+
+    public static final Long MODIFY_USER_ID = 2L;
+
+    @Test
+    public void modifyWithExistsIdShouldModifyUser() {
+
+        UserDto dto = new UserDto();
+        dto.setUser(NEW_USER_NAME);
+        dto.setPassword(NEW_USER_PASS);
+
+        restTemplate.exchange(LOCALHOST + port + SERVICE_PATH + "/" + MODIFY_USER_ID, HttpMethod.PUT, new HttpEntity<>(dto), Void.class);
+
+        ResponseEntity<List<UserDto>> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.GET, null, responseType);
+        assertNotNull(response);
+        assertEquals(2, response.getBody().size());
+
+        UserDto userSearch = response.getBody().stream().filter(item -> item.getId().equals(MODIFY_USER_ID)).findFirst().orElse(null);
+        assertNotNull(userSearch);
+        assertEquals(NEW_USER_NAME, userSearch.getUser());
+    }
+
+    public static final Long NOT_EXISTS_ID = 3L;
+
+    @Test
+    public void modifyWithNotExistIdShouldInternalError() {
+
+        UserDto dto = new UserDto();
+        dto.setUser(NEW_USER_NAME);
+        dto.setPassword(NEW_USER_PASS);
+
+        ResponseEntity<?> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH + "/" + NOT_EXISTS_ID, HttpMethod.PUT, new HttpEntity<>(dto), Void.class);
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+
+    public static final Long DELETE_USER_ID = 12L;
+
+    @Test
+    public void deleteWithExistsIdShouldDeleteUser() {
+
+        restTemplate.exchange(LOCALHOST + port + SERVICE_PATH + "/" + DELETE_USER_ID, HttpMethod.DELETE, null, Void.class);
+
+        ResponseEntity<List<UserDto>> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH, HttpMethod.GET, null, responseType);
+        assertNotNull(response);
+        assertEquals(2, response.getBody().size());
+    }
+
+    @Test
+    public void deleteWithNotExistsIdShouldInternalError() {
+
+        ResponseEntity<?> response = restTemplate.exchange(LOCALHOST + port + SERVICE_PATH + "/" + NEW_USER_ID, HttpMethod.DELETE, null, Void.class);
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
     }
 }
--- bioboost/src/test/java/com/cdtm/bioboost/user/UserTest.java
+++ bioboost/src/test/java/com/cdtm/bioboost/user/UserTest.java
@@ -1,14 +1,18 @@
 package com.cdtm.bioboost.user;
 
 import com.cdtm.bioboost.user.model.User;
+import com.cdtm.bioboost.user.model.UserDto;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -34,5 +38,49 @@ public class UserTest {
 
         assertNotNull(users);
         assertEquals(1, users.size());
+    }
+
+    public static final String USER_NAME = "u1";
+
+    @Test
+    public void saveNotExistsUserIdShouldInsert() {
+
+        UserDto userDto = new UserDto();
+        userDto.setUser(USER_NAME);
+
+        ArgumentCaptor<User> user = ArgumentCaptor.forClass(User.class);
+
+        userService.save(null, userDto);
+
+        verify(userRepository).save(user.capture());
+
+        assertEquals(USER_NAME, user.getValue().getUser());
+    }
+
+    public static final Long EXISTS_USER_ID = 1L;
+
+    @Test
+    public void saveExistsUserIdShouldUpdate() {
+
+        UserDto userDto = new UserDto();
+        userDto.setUser(USER_NAME);
+
+        User user = mock(User.class);
+        when(userRepository.findById(EXISTS_USER_ID)).thenReturn(Optional.of(user));
+
+        userService.save(EXISTS_USER_ID, userDto);
+
+        verify(userRepository).save(user);
+    }
+
+    @Test
+    public void deleteExistsUserIdShouldDelete() throws Exception {
+
+        User user = mock(User.class);
+        when(userRepository.findById(EXISTS_USER_ID)).thenReturn(Optional.of(user));
+
+        userService.delete(EXISTS_USER_ID);
+
+        verify(userRepository).deleteById(EXISTS_USER_ID);
     }
 }
